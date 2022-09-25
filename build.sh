c32d0a4c6dca468cd6862edb3b532e768893058d
Antoine Salon <antoine.salon96@gmail.com>
2022-09-25 09:56:28 -0700

    [CI] Add workflow dispatch support

--- build.sh
+++ build.sh
@@ -4,6 +4,24 @@ set -ev
 
 # source $HOME/.cargo/env
 export PATH=$PATH:~/.cargo/bin:$GOROOT/bin
-export PYENV_VERSION=3.7
 
-git --no-pager diff --name-only origin/master -- | grep "day-" | cut -d "/" -f1 | cut -d "-" -f2 | sort | uniq | xargs -I{} ./aoc run -fd {}
+run_from_diff() {
+    echo "$1" | grep "day-" | cut -d "/" -f1 | cut -d "-" -f2 | sort | uniq | xargs -I{} ./aoc run -fd {}
+}
+
+run_from_inputs() {
+    for day in $1; do echo "$day"; done | sort | uniq | xargs -I{} ./aoc run -fd {}
+}
+
+# Note: we cannot use "git branch --show-current" as GitHub rewrites the history in actions
+if [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ];
+then
+    run_from_inputs "$INPUT_DAYS"
+elif [ "$GITHUB_EVENT_NAME" == "pull_request" ];
+then
+    # Check the diff with master
+    run_from_diff "$(git --no-pager diff --name-only origin/master --)"
+else
+    # Check the diff of the last commit
+    run_from_diff "$(git --no-pager diff --name-only HEAD HEAD^ --)"
+fi
